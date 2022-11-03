56b918a052c0b705d4de54a843069934a1dda475
mathsudre <mathsudre@gmail.com>
2022-11-03 12:03:31 -0300

    feat: user/controller/service/test

---
+++ src/users/users.service.ts
@@ -0,0 +1,66 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from './../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { username, email } = createUserDto;
+
+    const uniqueUserName = await this.prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+    const uniqueUserEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (uniqueUserName || uniqueUserEmail) {
+      throw new BadRequestException('Username or Email is already being used');
+    }
+
+     
+    const newUser = await this.prisma.user.create({ data: createUserDto });
+
+    return newUser
+  }
+
+  async findAll() {
+    return await this.prisma.user.findMany();
+  }
+
+  async findOne(id: string) {
+    return await this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return await this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  async remove(id: string) {
+    const findUser = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!findUser) {
+      throw new NotFoundException('User not found, invalid ID');
+    }
+
+    return !!await this.prisma.user.delete({ where: { id } });
+  }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,118 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from './../src/prisma/prisma.service';
+import {
+  emailAlreadyUsed,
+  fakeUsers,
+  userCreateTest,
+  userNameAlreadyUsed,
+  userShape,
+} from './mocks/user/index';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    prisma = app.get<PrismaService>(PrismaService);
+
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+    await prisma.user.create({
+      data: fakeUsers[0],
+    });
+    await prisma.user.create({
+      data: fakeUsers[1],
+    });
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('GET ---> /users', () => {
+    it('Should be able to returns a list of users', async () => {
+      const { status, body } = await request(app.getHttpServer()).get('/users');
+      expect(status).toBe(200);
+      expect(body).toStrictEqual(expect.arrayContaining([userShape]));
+      expect(body).toHaveLength(2);
+      expect(body[0].id).toEqual(fakeUsers[0].id);
+      expect(body[1].id).toEqual(fakeUsers[1].id);
+    });
+  });
+
+  describe('GET ---> /users/:id', () => {
+    it('Return a user by their id', async () => {
+      const { status, body } = await request(app.getHttpServer()).get(
+        `/users/${fakeUsers[0].id}`,
+      );
+
+      expect(status).toBe(200);
+      expect(body).toStrictEqual(userShape);
+      expect(body.id).toEqual(fakeUsers[0].id);
+    });
+
+    it('Fails to return non existing user', async () => {
+      const { status, body } = await request(app.getHttpServer()).get(
+        `/users/100`,
+      );
+      expect(status).toBe(404);
+    });
+  });
+
+  describe('POST ---> /users', () => {
+    it('Should be able to create a user', async () => {
+      const beforeCount = await prisma.user.count();
+
+      const { status, body } = await request(app.getHttpServer())
+        .post('/users')
+        .send(userCreateTest);
+
+      const afterCount = await prisma.user.count();
+
+      expect(status).toBe(201);
+      expect(afterCount - beforeCount).toBe(1);
+    });
+
+    it('Failed to create a user with an username already being used', async () => {
+      const { status, body } = await request(app.getHttpServer())
+        .post('/users')
+        .send(userNameAlreadyUsed);
+
+      expect(status).toBe(400);
+    });
+  });
+
+  it('Failed to create a user with an email already being used', async () => {
+    const { status, body } = await request(app.getHttpServer())
+      .post('/users')
+      .send(emailAlreadyUsed);
+
+    expect(status).toBe(400);
+  });
+
+  describe('DELETE ---> /users/:id', () => {
+    it('Should be able to delete a user', async () => {
+      const { status, body } = await request(app.getHttpServer()).delete(
+        `/users/${fakeUsers[0].id}`,
+      );
+
+      expect(status).toBe(204);
+    });
+
+    it('Failed to delete a user with an invalid ID', async () => {
+      const { status, body } = await request(app.getHttpServer()).delete(
+        '/users/100',
+      );
+
+      expect(status).toBe(404);
+    });
   });
 });
