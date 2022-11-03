c3ca5d2e58b89cea601a69700e98ad751087119a
mathsudre <mathsudre@gmail.com>
2022-11-03 14:33:42 -0300

    feat: authMiddle in user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,11 +2,9 @@
 
 import { PrismaClient } from '@prisma/client';
 
-// initialize Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
-  // create two dummy articles
   const user1 = await prisma.user.upsert({
     where: { username: 'mathsudre' },
     update: {},
@@ -14,7 +12,7 @@ async function main() {
       username: 'mathsudre',
       avatar_url: '',
       email: 'math@email.com',
-      password: '123456',
+      password: 'Teste@123',
       steam_user: 'mathsudre',
       gamepass: true,
     },
@@ -27,7 +25,7 @@ async function main() {
       username: 'luwny',
       avatar_url: '',
       email: 'adam@email.com',
-      password: '123456',
+      password: 'Teste@123',
       steam_user: 'luwny',
       gamepass: true,
     },
@@ -36,13 +34,11 @@ async function main() {
   console.log({ user1, user2 });
 }
 
-// execute the main function
 main()
   .catch((e) => {
     console.error(e);
     process.exit(1);
   })
   .finally(async () => {
-    // close Prisma Client at the end
     await prisma.$disconnect();
   });
