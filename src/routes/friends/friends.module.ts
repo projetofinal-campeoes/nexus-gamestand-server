220f4341f58c4c06b773bc670a60a1d22782ff5e
ssidartaa <odssy351@gmail.com>
2022-11-06 11:01:55 -0500

    fix: integration with Friends's entity

--- src/routes/friends/friends.module.ts
+++ src/routes/friends/friends.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FriendsService } from './friends.service';
 import { FriendsController } from './friends.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [FriendsController],
-  providers: [FriendsService, PrismaService],
+  providers: [FriendsService],
+  imports: [PrismaModule],
 })
 export class FriendsModule {}
