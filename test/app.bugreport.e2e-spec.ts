16ab04a7c97de1bfb992fee3c2f41766428a5c9e
Adam N <adaamangelow@gmail.com>
2022-11-08 15:47:56 -0300

    wip: custom_games test

--- test/app.bugreport.e2e-spec.ts
+++ test/app.bugreport.e2e-spec.ts
@@ -34,12 +34,12 @@ describe('BugReportController (e2e)', () => {
     await app.init();
   });
 
-  describe('POST ---> /bug-report', () => {
+  describe('POST ---> /bug_report', () => {
     it('Should be able to create a bug report', async () => {
         const loginResponse = await request(app.getHttpServer()).post('/login').send(mockedUserLogin1)
         token = loginResponse.body.token
 
-        const { status, body } = await request(app.getHttpServer()).post('/bug-report').send(bugReportCreateData).set('Authorization', `Bearer ${token}`)
+        const { status, body } = await request(app.getHttpServer()).post('/bug_report').send(bugReportCreateData).set('Authorization', `Bearer ${token}`)
         report = body
 
         expect(status).toBe(201)
@@ -49,23 +49,23 @@ describe('BugReportController (e2e)', () => {
     })
 
     it('Should not be able to create a bug report without token', async () => {
-        const { status, body } = await request(app.getHttpServer()).post('/bug-report').send(bugReportCreateData)
+        const { status, body } = await request(app.getHttpServer()).post('/bug_report').send(bugReportCreateData)
 
         expect(status).toBe(401)
         expect(body).toHaveProperty('message')
     })
   })
 
-  describe('GET ---> /bug-report', () => {
+  describe('GET ---> /bug_report', () => {
     it('Should be able to list all bug reports', async () => {
-        const { status, body } = await request(app.getHttpServer()).get('/bug-report').set('Authorization', `Bearer ${token}`)
+        const { status, body } = await request(app.getHttpServer()).get('/bug_report').set('Authorization', `Bearer ${token}`)
         
         expect(status).toBe(200)
         expect(body).toHaveLength(1)
     })
 
     it('Should not be able to list all bug reports without token', async () => {
-        const { status, body } = await request(app.getHttpServer()).get('/bug-report')
+        const { status, body } = await request(app.getHttpServer()).get('/bug_report')
         
         expect(status).toBe(401)
         expect(body).toHaveProperty('message')
@@ -74,13 +74,13 @@ describe('BugReportController (e2e)', () => {
 
   describe('DELETE ---> /promotions/:id', () => {
     it('Should be able to delete a bug report', async () => {
-        const { status } = await request(app.getHttpServer()).delete(`/bug-report/${report.id}`).set('Authorization', `Bearer ${token}`)
+        const { status } = await request(app.getHttpServer()).delete(`/bug_report/${report.id}`).set('Authorization', `Bearer ${token}`)
 
         expect(status).toBe(204)
     })
 
     it('Should be able to delete a bug report without token', async () => {
-        const { status, body } = await request(app.getHttpServer()).delete(`/bug-report/${report.id}`)
+        const { status, body } = await request(app.getHttpServer()).delete(`/bug_report/${report.id}`)
 
         expect(status).toBe(401)
         expect(body).toHaveProperty('message')
