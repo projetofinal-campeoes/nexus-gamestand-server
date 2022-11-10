5162935ec2b73dabb1205afbea63e82225adacd3
Adam Neves <93692439+adamsnows@users.noreply.github.com>
2022-11-09 21:46:50 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
 
